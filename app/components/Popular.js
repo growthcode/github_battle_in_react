82304ec939ee92e50e2461620c40f3f02511baa9
growthcode <growthcode@gmail.com>
2017-08-29 09:01:00 -0700

    add api get call to fetch data for component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,9 +1,8 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api')
 
 function SelectLanguage (props) {
-  // stateless functional component
-  // the syntax can be written as a function rather than a whole class because it has no state
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
   return (
@@ -27,22 +26,75 @@ SelectLanguage.propTypes = {
   onSelect: PropTypes.func.isRequired,
 }
 
+function RepoGrid(props){
+  return (
+    <ul className="popular-list">
+      {props.repos.map(function(repo, index){
+        return (
+          <li key={repo.name} className="popular-item">
+            <div className="popular-rand">#{index+1}</div>
+            <ul className="space-list-items">
+              <li>
+                <img
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for ' + repo.owner.login}
+                />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
 
 class Popular extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedLanguage: 'All',
+      repos: null,
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage)
   }
   updateLanguage(lang) {
     this.setState(function () {
       return {
         selectedLanguage: lang,
+        repos: null,
       }
     });
+
+    // // Version 1, by explicitly using variable for context of "this"
+    // var popular = this
+    // api.fetchPopularRepos(lang).then(function(repos){
+      // popular.setState(function () {
+        // return {
+          // repos: repos,
+        // }
+      // });
+    // });
+
+    // Version 2, binding context of "this"
+    api.fetchPopularRepos(lang).then(function(repos){
+      this.setState(function () {
+        return {
+          repos: repos,
+        }
+      });
+    }.bind(this));
   }
   render() {
 
@@ -52,6 +104,12 @@ class Popular extends React.Component {
           selectedLanguage={ this.state.selectedLanguage }
           onSelect={ this.updateLanguage }
         />
+        {!this.state.repos ? <p>LOADING</p> :
+          <RepoGrid
+            repos={ this.state.repos }
+          />
+        }
+
       </div>
     )
   }
